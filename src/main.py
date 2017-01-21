6d459a547e647a0a795642c41e87d93df337a93b
Martin Jablečník <martin.jablecnik@email.cz>
2017-01-21 16:33:31 +0100

    added try/except

--- src/main.py
+++ src/main.py
@@ -15,12 +15,18 @@ from redis_words import get_redis_words
 from style import DocumentStyle
 
 
-
-
 def main():
+    def command(cmd):
+        if (text.split(" ")[0].upper() == cmd):
+            return True
+
+
     home = os.environ["HOME"]
     history = FileHistory(home+'/.rdcli-history')
     connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+    print("     *** Welcome in RDcli (CLI for Redis) ***")
+    print("You can exit program by type QUIT or shortcut C-D \n")
+    
 
     while True:
         try:
@@ -30,24 +36,28 @@ def main():
         except EOFError:
             break  # Control-D pressed.
 
-        messages = connection.execute_command(text)
-        if (text.split(" ")[0].upper() == "HGETALL"):
-            i=0
-            name = ""
-            for message in messages:
-                if (i % 2):
-                    print("{0}: \t\t\"{1}\"".format(name, message))
-                else:
-                    name = message
-                i+=1
-        elif (text.split(" ")[0].upper() == "COMMAND"):
-            for message in messages:
-                print message
+        try:
+            messages = connection.execute_command(text)
+            if command("HGETALL"):
+                i=0
+                name = ""
+                for message in messages:
+                    if (i % 2):
+                        print("{0}: \t\t\"{1}\"".format(name, message))
+                    else:
+                        name = message
+                    i+=1
+            elif command("COMMAND"):
+                for message in messages:
+                    print message
 
+            elif command("QUIT"):
+                break
 
-        else:
-            print(messages)
-
+            else:
+                print(messages)
+        except Exception as e:
+            print(e)
 
 
     print('GoodBye!')
